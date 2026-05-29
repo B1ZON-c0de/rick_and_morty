cec19c77f6ecebebc9201ec09cbd9fdbae70875a
roman <andreicko.roma@yandex.ru>
2026-05-29 23:20:19 +0700

    feat(main.go): добавил сервер

--- BE/main.go
+++ BE/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func charactersHandler(w http.ResponseWriter, r *http.Request) {
+	characters, err := ParseJSONFile("characters.json")
+	if err != nil {
+		respondWithError(w, err)
+		return
+	}
+	respondWithJSON(w, characters)
+}
+
+func episodeHandler(w http.ResponseWriter, r *http.Request) {
+	episodes, err := ParseJSONFile("episode.json")
+	if err != nil {
+		respondWithError(w, err)
+		return
+	}
+
+	respondWithJSON(w, episodes)
+}
+
+func locationsHandler(w http.ResponseWriter, r *http.Request) {
+	locations, err := ParseJSONFile("location.json")
+	if err != nil {
+		respondWithError(w, err)
+		return
+	}
+
+	respondWithJSON(w, locations)
+}
 
 func main() {
-	fmt.Print("Hello World")
+	http.HandleFunc("/characters", charactersHandler)
+	http.HandleFunc("/episodes", episodeHandler)
+	http.HandleFunc("/locations", locationsHandler)
+	fmt.Println("сервер запущен по пути http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Ошибка запуска сервера: %v", err)
+	}
+}
+
+func respondWithError(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	if err := json.NewEncoder(w).Encode(map[string]string{"error": err.Error()}); err != nil {
+		fmt.Errorf("Ошибка: %w", err)
+	}
+}
+
+func respondWithJSON(w http.ResponseWriter, dataJSON []map[string]any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(map[string][]map[string]any{"data": dataJSON}); err != nil {
+		fmt.Errorf("Ошибка: %w", err)
+	}
 }
