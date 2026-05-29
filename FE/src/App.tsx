6831d8a626318675a58ef87fcf118e0ab683478b
roman <andreicko.roma@yandex.ru>
2026-05-30 00:03:11 +0700

    feat: добавил cors

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    fetch("http://localhost:8080/api/episodes").then((res) =>
+      console.log(res.json()),
+    );
+  });
   return <div>HEllo</div>;
 };
 export default App;
