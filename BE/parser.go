7fbd843cf9f6903d85f4c7a106daf43a6dee78b7
roman <andreicko.roma@yandex.ru>
2026-05-29 21:51:32 +0700

    feat: добавил функция парсинга из файла .json

--- BE/parser.go
+++ BE/parser.go
@@ -1,4 +1,22 @@
 package main
 
-func ParseJSONFile() {
+import (
+	"encoding/json"
+	"os"
+)
+
+func ParseJSONFile(filename string) ([]map[string]any, error) {
+	var dataJSON []map[string]any
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.NewDecoder(file).Decode(&dataJSON); err != nil {
+		return nil, err
+	}
+
+	return dataJSON, nil
 }
+
--- BE/parser_test.go
+++ BE/parser_test.go
@@ -1,9 +1,43 @@
-package main_test
+package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestParser(t *testing.T) {
+	tempJSONFile, clearTempJSONFile := createTempJsonFile(t)
+	defer clearTempJSONFile()
 
+	got, err := ParseJSONFile(tempJSONFile.Name())
+
+	if err != nil {
+		t.Errorf("Не ожидаемая ошибка %v", err)
+	}
+
+	want := 2
+
+	if len(got) != want {
+		t.Errorf("Ожидали %v, Получили %v", want, got[0]["id"])
+	}
 }
+
+func createTempJsonFile(t *testing.T) (*os.File, func()) {
+	t.Helper()
+	tempFile, _ := os.CreateTemp("", "test.json")
+
+	dataJSON := `[
+	{"id": 1,"name":"roman"},
+	{"id": 2, "name":"ivan"}
+	]
+	`
+	tempFile.Write([]byte(dataJSON))
+
+	clearFile := func() {
+		tempFile.Close()
+		os.Remove(tempFile.Name())
+	}
+
+	return tempFile, clearFile
+}
+
