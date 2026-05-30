0917249ed1f268331bd99c7d6d24190b1ff626a9
roman <andreicko.roma@yandex.ru>
2026-05-30 20:22:17 +0700

    feat(main.ts): добавил BrowserRouter

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,14 +1,4 @@
-import { useEffect } from "react";
-
 const App = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:8080/api/characters");
-      const data = await res.json();
-      console.log(data);
-    };
-    fetchData();
-  });
   return <div>HEllo</div>;
 };
 export default App;
