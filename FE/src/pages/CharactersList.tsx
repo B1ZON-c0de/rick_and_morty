2114df503dc5b0a26b099be8938555aee8b477c0
roman <andreicko.roma@yandex.ru>
2026-05-31 00:36:57 +0700

    feat: добавил вывод списка персонажей

--- FE/src/pages/CharactersList.tsx
+++ FE/src/pages/CharactersList.tsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import type { ICharacter } from "../types";
+import { ROUTES } from "../route";
+
 export function CharactersList() {
-  return <div>CharacterList</div>;
+  const { isLoading, data, error } = useFetch<ICharacter[]>("/characters");
+
+  return (
+    <>
+      {isLoading ? (
+        "Загрузка"
+      ) : error ? (
+        error
+      ) : (
+        <ul>
+          {data.map((character) => (
+            <li key={character.id}>
+              <Link to={ROUTES.characterList + "/" + character.id}>
+                {character.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
