0cd69d4556ed4cd4a5b64cfa5a2c84216270b71c
roman <andreicko.roma@yandex.ru>
2026-05-30 20:33:51 +0700

    feat: добавил роутинг

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,4 +1,21 @@
+import { Routes, Route } from "react-router-dom"
+
 const App = () => {
-  return <div>HEllo</div>;
+  return <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/characters" element={
+        <CharactersList />
+      }>
+        <Route path=":id" element={<CharacterItem />} />
+      </Route>
+      <Route path="/locations" element={<LocationsList />}>
+        <Route path=":id" element={<LocationItem />} />
+      </Route>
+      <Route path="/episodes" element={<EpisodesList />}>
+        <Route path=":id" element={<EpisodeItem />} />
+        <Route />
+    </Routes>
+  </>
 };
 export default App;
