8a095c7d50e9564aeb21ef1f6814df0ae3ed69ff
roman <andreicko.roma@yandex.ru>
2026-05-30 21:17:33 +0700

    fix: вывел маршруты в отдельный файл

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,25 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import {
-  CharacterItem,
-  CharactersList,
-  LocationItem,
-  LocationsList,
-  EpisodeItem,
-  EpisodesList,
-  Home,
-} from "./pages";
+import { ROUTES } from "./route";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/characters" element={<CharactersList />} />
-        <Route path="/characters/:id" element={<CharacterItem />} />
-        <Route path="/locations" element={<LocationsList />} />
-        <Route path="/locations/:id" element={<LocationItem />} />
-        <Route path="/episodes" element={<EpisodesList />} />
-        <Route path="/episodes/:id" element={<EpisodeItem />} />
+        {Object.keys(ROUTES).map((key) => (
+          <Route path={ROUTES[key].path} element={ROUTES[key].path} />
+        ))}
       </Routes>
     </>
   );
