f0a4ecdb9e24a4b63e0f71dd47509cb1f2dad74d
roman <andreicko.roma@yandex.ru>
2026-06-01 00:33:43 +0700

    feat: добавил динамический вывод персонажей

--- FE/src/pages/CharacterItem.tsx
+++ FE/src/pages/CharacterItem.tsx
@@ -1,15 +1,31 @@
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import type { ICharacter } from "../types";
+
+function CharacterInfo({
+  name,
+  status,
+  species,
+  type,
+  gender,
+  image,
+}: ICharacter) {
+  return (
+    <>
+      <p>Имя: {name}</p>
+      <p>Статус: {status}</p>
+      <p>Род: {species}</p>
+      <p>Тип: {type}</p>
+      <p>Гендер: {gender}</p>
+      <img src={image} />
+    </>
+  );
+}
 
 export function CharacterItem() {
-  const { state } = useLocation();
+  const { id } = useParams();
+  const { data, isLoading, error } = useFetch<ICharacter>("/characters/" + id);
   return (
-    <div>
-      <h1>{state.name}</h1>
-      <p>{state.status}</p>
-      <p>{state.species}</p>
-      <p>{state.type}</p>
-      <p>{state.gender}</p>
-      <img src={state.image} />
-    </div>
+    <>{isLoading ? "Загрузка" : error ? error : <CharacterInfo {...data} />}</>
   );
 }
