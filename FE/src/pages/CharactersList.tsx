019f818c6211bd3af4a7f6c71492d6a660c0d49f
roman <andreicko.roma@yandex.ru>
2026-06-02 23:21:29 +0700

    style: добавил стилизацию к списку персонажей

--- FE/src/pages/CharactersList.tsx
+++ FE/src/pages/CharactersList.tsx
@@ -13,11 +13,16 @@ export function CharactersList() {
       ) : error ? (
         error
       ) : (
-        <ul>
+        <ul className="grid grid-cols-3 max-w-7xl mx-auto mt-6 gap-3">
           {data.map((character) => (
             <li key={character.id}>
               <Link to={ROUTES.characterList.path + "/" + character.id}>
-                {character.name}
+                <div className=" flex flex-col gap-6 rounded-md bg-gray-50 border-2 border-transparent hover:border-green-600 transition-all duration-300 p-4">
+                  <img className="w-full object-cover" src={character.image} />
+                  <h2 className="text-xl font-bold text-center">
+                    {character.name}
+                  </h2>
+                </div>
               </Link>
             </li>
           ))}
