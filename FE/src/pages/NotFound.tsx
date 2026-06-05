91ff3773cc10259569357083a3da3d1c66576a52
roman <andreicko.roma@yandex.ru>
2026-06-06 03:37:10 +0700

    style: простилизовал страцницу 404

--- FE/src/pages/NotFound.tsx
+++ FE/src/pages/NotFound.tsx
@@ -1,3 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 export const NotFound = () => {
-  return <div>404</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="min-h-screen flex flex-col gap-4 justify-center items-center">
+      <h1 className="text-9xl font-bold text-green-800">404</h1>
+      <button
+        className="btn-primary"
+        onClick={() => navigate("/", { replace: true })}
+      >
+        Вернуться на главную
+      </button>
+    </div>
+  );
 };
