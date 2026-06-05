6d304643e3dd4f210145fec2e7a445470833cc11
roman <andreicko.roma@yandex.ru>
2026-06-06 03:19:29 +0700

    fix: удалил index.ts в pages

--- FE/src/route.tsx
+++ FE/src/route.tsx
@@ -11,46 +11,58 @@ export const ROUTES = {
     name: "Главная",
     path: "/",
     element: lazy(() =>
-      import("./pages").then((module) => ({ default: module.Home })),
+      import("./pages/Home").then((module) => ({ default: module.Home })),
     ),
   },
   characterList: {
     name: "Персонажи",
     path: "/character",
     element: lazy(() =>
-      import("./pages").then((module) => ({ default: module.CharactersList })),
+      import("./pages/CharactersList").then((module) => ({
+        default: module.CharactersList,
+      })),
     ),
   },
   characterItem: {
     path: "/character/:id",
     element: lazy(() =>
-      import("./pages").then((module) => ({ default: module.CharacterItem })),
+      import("./pages/CharacterItem").then((module) => ({
+        default: module.CharacterItem,
+      })),
     ),
   },
   locationList: {
     name: "Локации",
     path: "/location",
     element: lazy(() =>
-      import("./pages").then((module) => ({ default: module.LocationsList })),
+      import("./pages/LocationsList").then((module) => ({
+        default: module.LocationsList,
+      })),
     ),
   },
   locationItem: {
     path: "/location/:id",
     element: lazy(() =>
-      import("./pages").then((module) => ({ default: module.LocationItem })),
+      import("./pages/LocationItem").then((module) => ({
+        default: module.LocationItem,
+      })),
     ),
   },
   episodeList: {
     name: "Эпизоды",
     path: "/episode",
     element: lazy(() =>
-      import("./pages").then((module) => ({ default: module.EpisodesList })),
+      import("./pages/EpisodesList").then((module) => ({
+        default: module.EpisodesList,
+      })),
     ),
   },
   episodeItem: {
     path: "/episode/:id",
     element: lazy(() =>
-      import("./pages").then((module) => ({ default: module.EpisodeItem })),
+      import("./pages/EpisodeItem").then((module) => ({
+        default: module.EpisodeItem,
+      })),
     ),
   },
 } as const satisfies Record<string, RouteConfig>;
